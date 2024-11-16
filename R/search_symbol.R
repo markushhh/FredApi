f11bff1854d80ff2aa2873ffea463e2b4a9cc3fd
markushhh <22667889+markushhh@users.noreply.github.com>
2024-11-16 11:29:53 +0100

    tidying search_symbol

--- R/search_symbol.R
+++ R/search_symbol.R
@@ -4,36 +4,36 @@
 #' @title search_symbol
 #' @description Search for the symbol of an economic data series that matches the search text.
 #' @param search_text A string containing the words with which you expect to find the id of the variable.
-#' @param must_contain A string like "Germany" to explicitly search for a variable which contains "Germany" in its title
+#' @param must_contain A string like "Germany" to explicitly search for a variable which contains "Germany" in its title. Currently allows one expression only.
 #' @examples search_symbol("Exports", "Mexico")
 #' @examples search_symbol("GDP", "Germany")
 #' @examples search_symbol("G", "France")
-#' @examples x <- search_symbol("Exports", "Mexico")
-#' @examples print(x)
 search_symbol <- function(search_text, must_contain = "") {
-
   search_text <- gsub(" ", "+", search_text, fixed = TRUE)
   must_contain <- gsub(" ", "+", must_contain, fixed = TRUE)
 
-  url = "https://api.stlouisfed.org/fred/series/search"
+  url <- "https://api.stlouisfed.org/fred/series/search"
   parameters <- list(
     "api_key" = Sys.getenv("API_FRED"),
     "file_type" = "json",
     "search_text" = search_text
   )
 
-  response <- httr::content(httr::GET(url, query = parameters), as = "parsed")
-  l = response$seriess
+  response <-
+    httr::content(httr::GET(url, query = parameters), as = "parsed") |>
+    purrr::pluck("seriess")
 
-  ll <- which(stringi::stri_detect_fixed(unlist(purrr::map(l, 4)), must_contain))
-  l <- l[ll]
+  symbols <- tibble::tibble(
+    id = response |> purrr::map("id") |> unlist(),
+    popularity = response |> purrr::map("popularity") |> unlist(),
+    title = response |> purrr::map("title") |> unlist()
+  ) |>
+    tidyr::drop_na() |>
+    dplyr::arrange(desc(popularity))
 
-  x <- data.frame(popularity = unlist(purrr::map(l, 14)),
-             id = unlist(purrr::map(l, 1)),
-             title = unlist(purrr::map(l, 4))
-  )
-  x <- x[order(x$popularity, decreasing = TRUE), ]
+  if (must_contain != "") {
+    symbols <- symbols |> dplyr::filter(title |> stringr::str_detect(must_contain))
+  }
 
-  return(x)
-
+  return(symbols)
 }
