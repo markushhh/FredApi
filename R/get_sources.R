7156b7c874e8a31e57f9d4177c74d129a3db5b80
markushhh <22667889+markushhh@users.noreply.github.com>
2024-11-16 13:29:46 +0100

    tidying get_sources

--- R/get_sources.R
+++ R/get_sources.R
@@ -4,20 +4,22 @@
 #' @title get_sources
 #' @description Get all sources of economic data.
 #' @examples get_sources()
-get_sources <- function() {
-
-  url = "https://api.stlouisfed.org/fred/sources"
+get_sources <- function(api_key = Sys.getenv("API_FRED")) {
+  url <- "https://api.stlouisfed.org/fred/sources"
   parameters <- list(
-    "api_key" = Sys.getenv("API_FRED"),
+    "api_key" = api_key,
     "file_type" = "json"
   )
 
-  response <- httr::content(httr::GET(url, query = parameters), as = "parsed")
-  l = response$sources
+  response <-
+    httr::GET(url, query = parameters) |>
+    httr::content(as = "parsed") |>
+    purrr::pluck("sources")
 
-  x = data.frame(id = unlist(purrr::map(l, 1)),
-             name = unlist(purrr::map(l, 4)))
+  sources <- tibble(
+    id   = response |> purrr::map("id") |> unlist(),
+    name = response |> purrr::map("name") |> unlist()
+  )
 
-  return(x)
-
+  return(sources)
 }
